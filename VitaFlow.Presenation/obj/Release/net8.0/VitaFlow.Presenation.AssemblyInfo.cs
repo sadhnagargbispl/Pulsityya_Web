235d28b2133a89b5a0da3a4873e606f767189e87
sadhnagargbispl <99010109+sadhnagargbispl@users.noreply.github.com>
2026-09-24 13:24:32 +0530

    1

--- VitaFlow.Presenation/obj/Release/net8.0/VitaFlow.Presenation.AssemblyInfo.cs
+++ VitaFlow.Presenation/obj/Release/net8.0/VitaFlow.Presenation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitaFlow.Presenation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+341ae14a1d85836b1f58cb7203a7ac79f344ac82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c202f7389cf7c9d8c7ec3d3833e9406667654302")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitaFlow.Presenation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitaFlow.Presenation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
